7af7eba49d81fa0ec4b697cc24c986a67c7a3ea1
SatoNaofumi <121pg0011@tokyo.amg.ac.jp>
2021-12-30 00:03:54 +0900

    ModeServer,ResourceServerエラー処理実装

--- GameBase.cpp
+++ GameBase.cpp
@@ -32,12 +32,12 @@ namespace AppFrame {
       };
       bool GameBase::Initialize(HINSTANCE hinstance) {
          //デバッグモードならウインドウモードに設定及び、デバッグログの出力を行う
-#ifdef _DEBUG
-         AppWindowMode(true);
-         OutputDebugLog(true);
-#else
+#ifndef _DEBUG
          AppWindowMode(false);
          OutputDebugLog(false);
+#else
+         AppWindowMode(true);
+         OutputDebugLog(true);
 #endif
          //画面サイズ設定
          auto [width, height, depth] = GraphSize();
@@ -62,7 +62,7 @@ namespace AppFrame {
 
          _pathServer = std::make_unique<Path::PathServer>();
 
-         _resJson = std::make_unique<Resource::LoadJson>(*this);
+         _loadJson = std::make_unique<Resource::LoadJson>(*this);
 
          return true;
       }
